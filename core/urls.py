cab8fc56874620cddd12fdb17b9e81716276f604
Sirojiddin Yakubov <yakubov9791999@gmail.com>
2024-04-24 11:24:02 +0500

    change CI/CD

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("health/", lambda _: HttpResponse('Healthy!')),
     path('users/', include('users.urls')),
     path('', include('shares.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
